f9f49d763fee433e364e8ed21f7c6f6e322f7e9d
HY <948166674@qq.com>
2018-01-02 17:05:12 +0800

    ceshi3

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ import Greeter from './Greeter';
 import './main.css';
 
 
-render(<Greeter />,document.getElementById("root"));
+render( <Greeter />,document.getElementById("root"));
 //const greeter = require("./Greeter.js");
 //
 //document.getElementById("root").appendChild(greeter());
